86a8652488ee0a770c1140cb5f7339f7acaa7a4a
Emre Unal <emreunal99@gmail.com>
2013-11-01 15:35:09 +0200

    Added node mailer and implemented mail logic

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var nodemailer = require('nodemailer');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000);
+app.use(express.bodyParser());
 
-console.log('Listening port 3000');
+var smtp = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+	user: "", /* mail address here  */
+	pass: "" /* password here */
+    }
+});
+
+app.get('/hello',function(req,res){
+    res.send('Hello World!');
+});
+
+app.post('/mail',function(req,res){
+    var from = req.body.from;
+    var to = req.body.to;
+    var subject = req.body.subject;
+    var message = req.body.message;
+    var body = "from:"+from+"\nto:"+to+"\n"+"subject:"+subject+"\n"+"message:"+message+"\n";
+    // sendResponse(res,body);
+    
+    var mailOptions = {
+	from: from,
+	to: to,
+	subject: subject,
+	text: message
+    }
+
+    smtp.sendMail(mailOptions, function(error,response) {
+	if( error ) {
+	    console.log(error);
+	} else {
+	    var body = "Message sent: "+response.message+"\n";
+	    console.log(body);
+	    res.send(body);
+	}
+    });
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
